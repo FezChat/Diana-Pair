195b41ea2301d06e7734c179bf02ddb869e866a0
FeeSpace <frediezra001@gmail.com>
2025-12-18 10:01:30 +0300

    Switch import from @fredi/baileys to @whiskeysocket/baileys

--- frediqr.js
+++ frediqr.js
@@ -14,7 +14,7 @@ const {
 	Browsers,
 	delay,
 	makeInMemoryStore, 
-} = require("@fredi/baileys");
+} = require("@whiskeysocket/baileys");
 
 function removeFile(FilePath) {
 	if (!fs.existsSync(FilePath)) return false;
